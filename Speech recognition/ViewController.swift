944475bdd160f5cc90e36a6eb4fc1ae7ad271677
RomainRui <romain.rui10@gmail.com>
2016-09-19 17:51:47 +0200

    clean code

--- Speech recognition/ViewController.swift
+++ Speech recognition/ViewController.swift
@@ -70,7 +70,6 @@ class ViewController: UIViewController {
     //-- Create preview methods
     func createPreview(size: CGSize, session: AVCaptureSession) -> AVCaptureVideoPreviewLayer? {
         previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        print(size)
         previewLayer?.frame.size = size
         previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
         previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.portrait
